244169e5235cfe32cbadf8a12e407fa38cd5c6b8
neelisettihariprasad <140797775+neelisettihariprasad@users.noreply.github.com>
2023-09-16 20:31:16 +0530

    Add files via upload

---
+++ WebDevelopment/HARIPRASAD/myscript.js
@@ -0,0 +1,7 @@
+function change_formats()
+{
+    document.getElementById("para1").style.fontSize="30pt";
+    document.getElementById("para1").style.fontFamily="Times New Roman";
+    document.getElementById("para1").style.borderBlockStyle="dotted";
+    document.getElementById("para1").style.color='red';
+}
